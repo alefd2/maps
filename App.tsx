8dd87bcbf52258b9a80338d90fbcb32b15e0380d
Alef oliveira <alefolivfeira@gmail.com>
2024-05-10 12:27:55 -0300

    fix

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
-import { Switch, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
@@ -14,72 +21,136 @@ import MapView, { LatLng, Marker, Polyline } from "react-native-maps";
 
 export default function App() {
   const [location, setLocation] = useState<LocationObject | null>(null);
+  const [reset, setReset] = useState<boolean>(false);
+  const [currentLocation, setCurrentLocation] = useState<LatLng | null>(null);
   const [route, setRoute] = useState<LatLng[]>([]);
   const [getRoute, setGetRoute] = useState<boolean>(true);
+  const [intervalo, setIntervalo] = useState<number>(3000);
+  const [distance, setDistance] = useState<number>(10);
 
   const mapRef = useRef<MapView>(null);
 
   async function requestPermissionAsync() {
     const { granted } = await requestForegroundPermissionsAsync();
-
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
       setLocation(currentPosition);
+      clearLatLong();
+      setRoute([location?.coords!]);
     }
   }
+
   useEffect(() => {
     requestPermissionAsync();
-  }, []);
+  }, [reset]);
 
-  const isGetRoute = () => {
+  const handleSalveLatLng = () => {
+    setRoute((prevRoute) => [...prevRoute, currentLocation!]);
+  };
+
+  const handleRestore = () => {
+    setReset(!reset);
+  };
+
+  const clearLatLong = () => {
     setRoute([]);
-    setGetRoute(true);
-  };
-
-  const isCancelGetRoute = () => {
-    setGetRoute(false);
-  };
-
-  const handleGetRoute = (value?: boolean) => {
-    if (value) {
-      return isGetRoute();
-    }
-    return isCancelGetRoute();
   };
 
   useEffect(() => {
     watchPositionAsync(
       {
         accuracy: LocationAccuracy.Highest,
-        timeInterval: 3000,
-        distanceInterval: 5,
+        timeInterval: intervalo,
+        distanceInterval: distance,
       },
       (response) => {
-        // console.log(response);
+        console.log(response.coords);
         mapRef.current?.animateCamera({
           pitch: 70,
           center: response.coords,
         });
 
-        if (getRoute) {
-          setRoute((prevRoute) => [...prevRoute, response.coords]);
-        }
+        setCurrentLocation(response.coords);
       }
     );
-  }, [isGetRoute, isCancelGetRoute]);
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.switchContainer}>
-        <Switch
-          trackColor={{ false: "#777575", true: "#115500" }}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={(value) => handleGetRoute(value)}
-          value={getRoute}
-        />
-        <Text>{getRoute ? "ativo" : "Inativo "} |</Text>
-
-        {route.length >= 0 && <Text>Pontos salvos: {route.length} </Text>}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.actionWrapper}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#b4a201",
+            padding: 10,
+            borderRadius: 5,
+            elevation: 5,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            display: "flex",
+            margin: 10,
+            width: "auto",
+          }}
+          onPress={() => handleRestore()}
+        >
+          <MaterialCommunityIcons name="restore" size={30} color="#fdfdfd" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#01b452",
+            padding: 10,
+            borderRadius: 5,
+            elevation: 5, // Adicionando sombra
+            shadowColor: "#000", // Cor da sombra
+            shadowOffset: { width: 0, height: 2 }, // Offset da sombra
+            shadowOpacity: 0.25, // Opacidade da sombra
+            shadowRadius: 3.84, // Raio da sombra
+            display: "flex",
+            margin: 10,
+            width: "auto",
+          }}
+          onPress={() => handleSalveLatLng()}
+        >
+          <MaterialCommunityIcons
+            name="map-marker-check"
+            size={30}
+            color="#fdfdfd"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#b42b01",
+            elevation: 5, // Adicionando sombra
+            shadowColor: "#000", // Cor da sombra
+            shadowOffset: { width: 0, height: 2 }, // Offset da sombra
+            shadowOpacity: 0.25, // Opacidade da sombra
+            shadowRadius: 3.84, // Raio da sombra
+            padding: 10,
+            borderRadius: 5,
+            margin: 10,
+          }}
+          onPress={() => clearLatLong()}
+        >
+          <MaterialCommunityIcons name="trash-can" size={30} color="#fdfdfd" />
+        </TouchableOpacity>
+        {route.length >= 0 && (
+          <Text
+            style={{
+              backgroundColor: "#ffffff",
+              padding: 10,
+              borderRadius: 5,
+              elevation: 5, // Adicionando sombra
+              shadowColor: "#000", // Cor da sombra
+              shadowOffset: { width: 0, height: 2 }, // Offset da sombra
+              shadowOpacity: 0.25, // Opacidade da sombra
+              shadowRadius: 3.84, // Raio da sombra
+              fontSize: 10,
+            }}
+          >
+            Pontos salvos: {route.length}{" "}
+          </Text>
+        )}
       </View>
       {location && (
         <MapView
@@ -125,6 +196,6 @@ export default function App() {
             ))}
         </MapView>
       )}
-    </View>
+    </SafeAreaView>
   );
 }
